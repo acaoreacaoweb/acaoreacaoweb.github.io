05fd919f4ff8becaa3fe5853dbbce4f282eea5f2
acaoreacaoweb <acaoreacaoweb@gmail.com>
2019-12-12 00:00:13 -0300

    Updates

---
+++ precache-manifest.8681019cdce167e8437a06ccc02c8898.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "657eb5814c68b0f80708a50fa6091d08",
+    "url": "/acaoreacaoweb/index.html"
+  },
+  {
+    "revision": "94fe10922a2e46753636",
+    "url": "/acaoreacaoweb/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "dfeb17ae3903b7e9878a",
+    "url": "/acaoreacaoweb/static/css/main.7b1d5bff.chunk.css"
+  },
+  {
+    "revision": "94fe10922a2e46753636",
+    "url": "/acaoreacaoweb/static/js/2.c96cfbb1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/acaoreacaoweb/static/js/2.c96cfbb1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dfeb17ae3903b7e9878a",
+    "url": "/acaoreacaoweb/static/js/main.5f74d744.chunk.js"
+  },
+  {
+    "revision": "1954ed1fd2f2232086fd",
+    "url": "/acaoreacaoweb/static/js/runtime-main.d9ac701b.js"
+  }
+]);
